0808825a3b2c455e16b92425641dfab30e464f7a
Chanchal <itsc151225@gmail.com>
2026-01-16 17:44:51 +0530

    store backup

--- sample.tf
+++ sample.tf
@@ -13,7 +13,7 @@ provider "aws" {
 resource "aws_instance" "myinstance" {
   ami = "ami-0ecb62995f68bb549"
   instance_type = "t3.micro"
-  vpc_security_group_ids = "sg-0ae44f0b95760a73a"
+  vpc_security_group_ids = ["sg-0ae44f0b95760a73a"]
   key_name = "rsa"
   availability_zone = "us-east-1a"
   tags = {
