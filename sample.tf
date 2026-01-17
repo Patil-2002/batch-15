cf0f0235f827d3c86d51d761efc3a98ea1de07c3
Chanchal <itsc151225@gmail.com>
2026-01-17 10:41:53 +0530

    pass the variable

--- sample.tf
+++ sample.tf
@@ -11,14 +11,26 @@ provider "aws" {
   
 }
 resource "aws_instance" "myinstance" {
-  ami = "ami-0ecb62995f68bb549"
-  instance_type = "t3.micro"
+  ami = var.ami_id
+  instance_type = var.instance_type
   vpc_security_group_ids = ["sg-0ae44f0b95760a73a"]
-  key_name = "rsa"
+  key_name = var.key_name
   availability_zone = "us-east-1a"
   tags = {
     "name" = "myinstance"
   }
+  
+}
 
-
+variable "ami_id" {
+  default = "ami-0ecb62995f68bb549"
+  
+}
+variable "instance_type" {
+  default = "t3.micro"
+  
+}
+variable "key_name" {
+  default = "rsa"
+  
 }
